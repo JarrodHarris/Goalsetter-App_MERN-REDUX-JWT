d650d1975d0a342ee66f11846b1c1774eaed33dc
Arogorn64 <70305469+Arogorn64@users.noreply.github.com>
2023-01-23 22:27:15 +1100

    initial commit

---
+++ backend/middleware/errorMidlleware.js
@@ -0,0 +1,16 @@
+//middleware functions that execute during a response cycle
+//this is to override the express error handler
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500
+
+    res.status(statusCode)
+
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+    })
+}
+
+module.exports = {
+    errorHandler,
+}
